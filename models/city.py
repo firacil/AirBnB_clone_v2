45dad15e4c90efaf507dc2e281788e175eac1e36
firacil <firaolmekbib511@gmail.com>
2024-05-18 15:03:30 -0700

    updated

--- models/city.py
+++ models/city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 import os
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes the class"""
+        """intializes the class"""
         super().__init__(*args, **kwargs)
