50104b05744793228b3e4907a892f065763f9233
firacil <firaolmekbib511@gmail.com>
2024-05-18 15:00:29 -0700

    updated

--- models/city.py
+++ models/city.py
@@ -9,15 +9,9 @@ from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade='all, delete, delete-orphan',
-        backref='cities'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes the class"""
+        super().__init__(*args, **kwargs)
