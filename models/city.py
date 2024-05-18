ac66b8ad9d680cf0694406208d838b192f195a6d
firacil <firaolmekbib511@gmail.com>
2024-05-18 15:59:11 -0700

    updated

--- models/city.py
+++ models/city.py
@@ -10,14 +10,14 @@ from models.base_model import BaseModel, Base
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    name = Column(String(128), nullable=False)
     state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+        String(60),
+        ForeignKey('states.id'),
+        nullable=False
+        )
     places = relationship(
         'Place',
-        cascade='all, delete, delete-orphan',
-        backref='cities'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+        cascade='all, delete-orphan',
+        backref='city'
+        )
--- models/state.py
+++ models/state.py
@@ -11,22 +11,19 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
+    name = Column(String(128), nullable=False)
+    cities = relationship(
             'City',
-            cascade='all, delete, delete-orphan',
+            cascade='all, delete-orphan',
             backref='state'
         )
-    else:
-        @property
-        def cities(self):
-            """Returns the cities in this State"""
-            from models import storage
-            cities_in_state = []
-            for value in storage.all(City).values():
-                if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+
+    @property
+    def cities(self):
+        """Returns the cities in this State"""
+        from models import storage
+        cities_in_state = []
+        for value in storage.all(City).values():
+            if value.state_id == self.id:
+                cities_in_state.append(value)
+        return cities_in_state
