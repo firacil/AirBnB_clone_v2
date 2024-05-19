64dcbc01b96d8878d3051e23676c5a37fa3a8326
firacil <firaolmekbib511@gmail.com>
2024-05-19 13:22:29 -0700

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def display_states():
     """Displays a list of all State objects present in DBStorage"""
-    all_states = storage.all(State)
-    states = sorted(all_states.values(), key=lambda state: state.name)
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
@@ -22,5 +21,4 @@ def teardown(self):
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
